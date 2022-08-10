6a7503ed4432a5d6e874c918c5fa2ea91546cbf2
ViktorJavaWeb <free.ukrainev@gmail.com>
2022-08-10 20:51:32 +0300

    firs commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,119 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+
+    <button @click="fetchGet">Заповнити таблицю</button>
+    <table class="post">
+      <thead>
+      <th>nameProduct</th>
+      <th>nameBrand</th>
+      <th>nameModel</th>
+      <th>nameColor</th>
+      <th>nameDescription</th>
+      <th>foto</th>
+      <th>update/delete</th>
+      </thead>
+      <tbody>
+      <tr v-for="post in posts" :key="post.id">
+        <td>{{ post.product }}</td>
+        <td>{{ post.brand }}</td>
+        <td>{{ post.model }}</td>
+        <td>{{ post.color }}</td>
+        <td>{{ post.descriptionProduct }}</td>
+        <td>{{ post.photoProduct }}</td>
+        <td><button>updateProduct</button><button>deleteProduct</button></td>
+      </tr>
+      </tbody>
+    </table>
+
+
+  </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  methods: {
+    async fetchGet() {
+      try {
+        const response = await axios.get('http://localhost:8080/prod/all');
+
+       this.ar = response.data ;
+
+        console.log(this.ar);
+
+        for (var i = 0; i < this.ar.length; i++) {
+          var arS = {};
+
+          arS['product'] = this.ar[i]['product'];
+          console.log(arS['nameProduct']);
+
+          arS['brand'] = this.ar[i]['brand'];
+          console.log(arS['brand']);
+
+          arS['model'] = this.ar[i]['model'];
+          console.log(arS['model']);
+
+          arS['color'] = this.ar[i]['color'];
+          console.log(arS['color']);
+
+          arS['description'] = this.ar[i]['description'];
+          console.log(arS['description']);
+
+        /*  arS['photoProduct'] = this.ar[i]['photoProduct'];
+          console.log(arS['photoProduct']);*/
+
+          this.posts[i] = arS;
+        }
+
+
+      } catch (e) {
+        alert('Error')
+      }
+    }
   }
+
 }
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
+
+
+table {
+  width: 50%; /* Ширина таблицы */
+  border: 4px double black; /* Рамка вокруг таблицы */
+  border-collapse: collapse; /* Отображать только одинарные линии */
+}
+
+th {
+  text-align: left; /* Выравнивание по левому краю */
+  background: #ccc; /* Цвет фона ячеек */
+  padding: 5px; /* Поля вокруг содержимого ячеек */
+  border: 1px solid black; /* Граница вокруг ячеек */
+}
+
+td {
+  padding: 5px; /* Поля вокруг содержимого ячеек */
+  border: 1px solid black; /* Граница вокруг ячеек */
+}
+
+.btn {
+  padding: 2px; /* Поля вокруг содержимого ячеек */
+  border: 1px solid black; /* Граница вокруг ячеек */
+  background: #ccc; /* Цвет фона ячеек */
+
+}
+
 </style>
